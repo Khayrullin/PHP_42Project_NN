eb1d55bd70d903f02301a3c99b16ca51b359ce3d
Bulat Khayrullin <habar.2704@mail.ru>
2018-04-27 20:03:23 +0300

    Spring Rt update

--- src/InputLayer.php
+++ src/InputLayer.php
@@ -1,18 +1,76 @@
 <?php
+
 class InputLayer {
 
-     private $_trainset=array(array(array(0,0),array(0,1)),
-			     array(array(0,1),array(1,0)),
-			     array(array(1,0),array(1,0)),
-			     array(array(1,1),array(0,1)));
-    public function getTrainset($key)
-     {
-      return $this->_trainset[$key];
-     }
+    private $_trainset = array();
+    private $_testset = array();
 
-    public function getCountTrainset()
-     {
-      return count($this->_trainset);
-     }
+    public function __construct($nm) {
+        $filename = $nm . "Set.png";
+        $size = getimagesize($filename);
+        $width = $size[0];
+        $height = $size[1];
+        $im = imagecreatefrompng($filename);
+
+        switch ($nm) {
+            case NetworkMode::TRAIN: {
+                    for ($y = 0; $y < ($height / 5); $y++) {
+                        for ($x = 0; $x < ($width / 3); $x++) {
+                            $this->_trainset[$x + $y * ($width / 3)][0] = array_fill(0, 15, null);
+                            $this->_trainset[$x + $y * ($width / 3)][1] = (integer) $y;
+                            for ($m = 0; $m < 5; $m++) {
+                                for ($n = 0; $n < 3; $n++) {
+                                    $this->_trainset[$x + $y * ($width / 3)][0][$n + 3 * $m] = (integer)
+                                            ((imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["red"] +
+                                            imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["green"] +
+                                            imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["blue"]) / 765);
+                                }
+                            }
+                        }
+                    }
+
+                    for ($n = (count($this->_trainset) - 1); $n >= 1; $n--) {
+                        $j = rand(0, $n);
+                        $temp = $this->_trainset[$n];
+                        $this->_trainset[$n] = $this->_trainset[$j];
+                        $this->_trainset[$j] = $temp;
+                    }
+                    break;
+                }
+            case NetworkMode::TEST: {
+                    for ($y = 0; $y < ($height / 5); $y++) {
+                        for ($x = 0; $x < ($width / 3); $x++) {
+                            $this->_testset[$x + $y * ($width / 3)][0] = array_fill(0, 15, null);
+                            $this->_testset[$x + $y * ($width / 3)][1] = (integer) $y;
+                            for ($m = 0; $m < 5; $m++) {
+                                for ($n = 0; $n < 3; $n++) {
+                                    $this->_testset[$x + $y * ($width / 3)][0][$n + 3 * $m] = (integer)
+                                            ((imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["red"] +
+                                            imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["green"] +
+                                            imagecolorsforindex($im, imagecolorat($im, ($n + 3 * $x), ($m + 5 * $y)))["blue"]) / 765);
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+        }
+    }
+
+    public function getTrainset($key) {
+        return $this->_trainset[$key];
+    }
+
+    public function getCountTrainset() {
+        return count($this->_trainset);
+    }
+
+    public function getTestset($key) {
+        return $this->_testset[$key];
+    }
+
+    public function getCountTestset() {
+        return count($this->_testset);
+    }
+
 }
-?>
